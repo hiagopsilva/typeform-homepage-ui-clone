7dd34a95d10978eef5896fb86a2246f9ec53210d
hiago <hiagohps360@gmail.com>
2021-01-28 15:18:55 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import { useTransform, useViewportScroll } from 'framer-motion';
+
 import { Container } from './styles';
 
 const Header: React.FC = () => {
-  return (
-    <Container>
-      <h1>Header</h1>
-    </Container>
+  const { scrollYProgress } = useViewportScroll();
+
+  const headerY = useTransform(
+    scrollYProgress,
+    [0.188, 0.198],
+    ['0%', '-100%'],
   );
+  return <Container style={{ y: headerY }} />;
 };
 
 export default Header;

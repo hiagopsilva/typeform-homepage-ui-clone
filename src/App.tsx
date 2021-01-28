0e11b5a471812d1965681c66507c15643e4cfd5b
hiago <hiagohps360@gmail.com>
2021-01-28 15:19:35 -0300

    chore: updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import Header from './components/Header';
+
+import { FirstAndSecond, Header } from './components';
+import { GlobalStyles, Main, Section, Sticky } from './styles';
 
 function App() {
-  return <div className="App">
-    <Header />
-    Type form ui clone</div>;
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
+  return (
+    <>
+      <GlobalStyles />
+
+      <Header />
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as FirstAndSecond } from './FirstAndSecond';
+export { default as Header } from './Header';
